9a372a8eb23f552f8106d05038404b511415d13d
Vignesh <reactivicky@gmail.com>
2023-10-31 15:27:14 +0530

    tailwind config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cardColor: "#ffffff",
+        cardBorder: "#e6e6e6",
+        dark: "#212121",
+        white: "#fafafa",
+        error: "#d86161",
+        placeholder: "#7a7a7a",
+        primary: "#1597e4",
+      },
+      height: {
+        formHeight: "564px",
+      },
+      width: {
+        formWidth: "577px",
+      },
+      borderRadius: {
+        formBorderRadius: "10px",
+      },
+      padding: {
+        formPadding: "32px",
+      },
+      gap: {
+        formGap: "24px",
+      },
+      fontSize: {
+        formHeading: "20px",
+        pageNumber: "16px",
+      },
+      fontWeight: {
+        pageNumber: "500",
+      },
+    },
   },
   plugins: [],
-}
+};
