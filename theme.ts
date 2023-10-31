8ae5dfa0c0656116853b33de8814131839c47c11
Vignesh <reactivicky@gmail.com>
2023-10-31 14:45:34 +0530

    initial project setup

---
+++ theme.ts
@@ -0,0 +1,11 @@
+const colors = {
+  cardColor: '#ffffff',
+  cardBorder: '#e6e6e6',
+  dark: '#212121',
+  white: '#fafafa',
+  error: '#d86161',
+  placeholder: '#7a7a7a',
+  primary: '#1597e4'
+}
+
+export { colors }
